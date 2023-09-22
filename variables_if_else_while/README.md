0a94070d29942ee1b3531ed447d83e792db65322
Ashley Chapman <7186@holbertonstudents.com>
2023-09-21 20:31:27 -0700

    0-positive_or_negative.c

---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+These are tasks for using varibales, if, else, and while in C programming language
---
+++ variables_if_else_while/for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i <= 10; i++){
+
+		if (i == 6)
+	       	{
+			break;
+		}
+		printf("Number %d\n", i);
+	}
+
+	return 0;
+}
