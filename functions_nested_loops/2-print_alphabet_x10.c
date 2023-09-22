251a81c3048d3ece0fccefed3265bd114bff314e
Ashley Chapman <7186@holbertonstudents.com>
2023-09-22 16:28:35 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet - print the alphabet 10 times
- * Return: void here
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
