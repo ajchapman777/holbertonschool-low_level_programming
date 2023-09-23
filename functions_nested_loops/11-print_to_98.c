a58928d512b34032f92171085bec99fd7b6cb9d4
Ashley Chapman <7186@holbertonstudents.com>
2023-09-23 12:11:03 -0700

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,17 +6,23 @@
  * Return: Always 0
  */
 void print_to_98(int n)
-
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		while (n < 98)
 		{
-			if (n == 98)
-				printf("%d", n);
-			else
-				printf("%d, ", n);
+			printf("%d, ", n);
+			n++;
 		}
-		printf("\n");
+		printf("%d", n);
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", n);
 	}
 }
