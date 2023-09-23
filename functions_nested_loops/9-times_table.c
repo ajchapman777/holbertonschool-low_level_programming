cefc29caae91f82d1fadc30f52e41643266fd823
Ashley Chapman <7186@holbertonstudents.com>
2023-09-22 23:29:11 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,37 +1,35 @@
 #include "main.h"
 /**
- * times_table - prints the 9 times table
- * Description: prints the 9 times table
- * Return: void
+ * main - check the code for Holberton School students.
+ * times_table - Prints the 9 times table
+ * Description: This function prints the 9 times table in a specific format.
+ *              It includes commas, spaces, and newlines for proper alignment.
+ * Return: void (no return value)
  */
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int row, column, product;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
 		{
 			product = (row * column);
-			tens = product / 10;
-			ones = product % 10;
+
 			if (column == 0)
 			{
 				_putchar('0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
 				if (product < 10)
 					_putchar(' ');
+				_putchar((product % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(ones = 0);
 		}
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(tens + '0');
-			_putchar(ones = '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
