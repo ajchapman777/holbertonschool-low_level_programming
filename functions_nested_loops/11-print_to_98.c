b5f5b557021c995a1f3f8fa01d06ede1d14d8321
Ashley Chapman <7186@holbertonstudents.com>
2023-09-23 14:06:17 -0700

    11-98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,17 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	while (n < 98)
+	for (; n < 98; n++)
 	{
 	printf("%d, ", n);
-	n++;
 	}
-	printf("%d", n);
 	}
 	else
 	{
-	while (n > 98)
+	for (; n > 98; n--)
 	{
 	printf("%d, ", n);
-	n--;
 	}
-	printf("%d\n", n);
 	}
+	printf("98\n");
 }
