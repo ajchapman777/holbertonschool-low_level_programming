54529255540512600a6c3bbc364edab7f60b9b06
Ashley Chapman <7186@holbertonstudents.com>
2023-10-01 16:21:26 -0700

    0-98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that resets the calue of an int to 98
+ * @n: the value of a dereferencing pointer
+ * Return: nth
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+these are the tasks for pointers, arrays , and strings in C programming language
