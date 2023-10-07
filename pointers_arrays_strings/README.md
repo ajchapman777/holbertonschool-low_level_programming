ded3648c081178717dc5e1e1907c70064f42030a
Ashley Chapman <7186@holbertonstudents.com>
2023-10-07 12:48:46 -0700

    3-strcmp

--- pointers_arrays_strings/README.md
+++
@@ -1,3 +0,0 @@
-these are the tasks for pointers, arrays , and strings in C programming language
-
-these are also the tasks for more pointers, arrays , and strings
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -7,7 +7,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
 char *cap_string(char *);
+
 
 #endif
