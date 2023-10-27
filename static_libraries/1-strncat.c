eb71d63f7d4814d77aecf2e393a99b4bc4206cb9
Ashley Chapman <ajchapman777@gmail.com>
2023-10-26 20:51:05 -0700

    static

--- static_libraries/1-strncat.c
+++ static_libraries/1-strncat.c
@@ -1,1 +1,20 @@
-char *_strncat(char *dest, char *src, int n);
+#include "main.h"
+/**
+ * _strncat - concantenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
