dc66da866f8c47cf243df7dd0ae0b38666968757
Ashley Chapman <ajchapman777@gmail.com>
2023-10-29 22:56:29 -0700

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (size <= 0)
 		return (-1);
-	for (i = 0; 1 < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
 			return (i);
