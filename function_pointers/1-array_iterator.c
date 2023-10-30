a57395f451c089427cf44c9eb0bf66478d882e7d
Ashley Chapman <ajchapman777@gmail.com>
2023-10-29 22:18:49 -0700

    0

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap two integers
+ * @2: value 1
+ * @b: value 2
+ * Return: void
+ */
+void swap_int(int *s, int *b)
+{
+	int c:
+
+		c = *a;
+		*a = *b;
+		*b = c;
+}
