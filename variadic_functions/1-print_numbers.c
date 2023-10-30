7c5d981a4220ff60e057d95af064ec6c1bd1e6cc
Ashley Chapman <ajchapman777@gmail.com>
2023-10-30 11:50:27 -0700

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,11 +11,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arg;
 
 	va_start(arg, n);
+
 	for (i = 0; i < n; i++)
 	{
 		if (separator != NULL)
 		{
 			printf("%d", va_arg(arg, int));
+			
+			if (i != n - 1)
 			printf("%s", separator);
 		}
 		else
