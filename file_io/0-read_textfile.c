cc8cb1343e8b1a553a3381dbb9e190f5a5a9d9ea
ajchapman777 <ajchapman777@gmail.com>
2023-11-13 20:20:52 -0800

    deleted white space

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -31,8 +31,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(file_d);
 	if (lenr == -1)
 	{
-		free (buffer);
-		return (0);
+		free(buffer);
+		return(0);
 	}
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free (buffer);
