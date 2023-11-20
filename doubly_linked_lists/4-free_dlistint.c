910c6dfa736c820471d2742a795590b810f97b4e
ajchapman777 <ajchapman777@gmail.com>
2023-11-20 14:20:43 -0800

    4-free list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * free_dlist - Frees a linked dlistint_t list
+ * free_dlistint - Frees a linked dlistint_t list
  * @head: The head of the dlistint_t list
  */
 void free_dlistint(dlistint_t *head)
