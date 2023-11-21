e7c4080a5e9c3fe1aceadbc3779cf49f6c762db6
ajchapman777 <ajchapman777@gmail.com>
2023-11-20 21:56:01 -0800

    removed whitespace

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_get - Retrieves a value associated with a key
  * @ht: The hash table to look into
- * @key: the key 
+ * @key: the key
  * Return: The value associated with the element, or NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
